90a2b46fda062c105e7f8e62ac04fd2a32277bc4
Alexxx180 <alexx120702@gmail.com>
2024-07-13 15:52:09 +0300

    wall collision finished

--- assets/systems/input/push.gd
+++ assets/systems/input/push.gd
@@ -1,28 +1,48 @@
 extends Area2D
 
 var power: int = 4
-var right: Vector2
+var tackle: float = 4
+var box: Dictionary
+
+@onready var walls: StaticBody2D = $walls
+@onready var timer: Timer = $feedback
 
 func _ready():
-	right = position + $interaction.shape.size
+	var right: Vector2 = position + $interaction.shape.size
+	box = {
+		'axis': 'x', 'touch': 1.2,
+		'pos': { 'x': position.x, 'y': position.y },
+		'right': { 'x': right.x, 'y': right.y }
+	}
 
-func _push_horizontal(tackle: int):
-	position.x += tackle
-	right.x += tackle
+func _push() -> float:
+	var axis: String = box['axis']
+	box['pos'][axis] += tackle
+	box['right'][axis] += tackle
+	return box['pos'][axis]
 
-func _push_forward(tackle: int):
-	position.y += tackle
-	right.y += tackle
-
-func _push(vertical: Vector2, side: int):
+func _push_forward(vertical: Vector2, side: int):
 	if vertical.y > -power and vertical.y < power:
-		_push_forward(power * side)
+		box['axis'] = 'y'
+		tackle = power * side
+		position.y = _push()
+		tackle *= -box['touch']
 	elif vertical.x > -power and vertical.x < power:
-		_push_horizontal(power * side)
+		box['axis'] = 'x'
+		tackle = power * side
+		position.x = _push()
+		#_push_horizontal(resist)
+		tackle *= -box['touch']
+
+func _enable(): walls.process_mode = Node.PROCESS_MODE_DISABLED
 
 func _on_push(hero: Node2D):
-	var bottom: Vector2 = Vector2(right.x - hero.position.x, right.y - hero.position.y)
-	var top: Vector2 = Vector2(position.x - hero.right.x, position.y - hero.right.y)
-
-	_push(bottom, -1)
-	_push(top, 1)
+	if not hero is CharacterBody2D or walls.process_mode == Node.PROCESS_MODE_INHERIT:
+		_push()
+		walls.process_mode = Node.PROCESS_MODE_INHERIT
+		timer.start()
+	else:
+		var bottom = Vector2(box['right'].x - hero.position.x, box['right'].y - hero.position.y)
+		var top = Vector2(position.x - hero.right.x, position.y - hero.right.y)
+		_push_forward(bottom, -1)
+		_push_forward(top, 1)
