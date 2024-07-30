898169688c402b1b00329680b3c5360bf061eda2
Alexxx180 <Alexxx180@github.com>
2024-07-30 11:59:16 -0400

    horizontal jumps mechanic simplified, there can be more than 2 obstacles on the track

--- assets/systems/input/platforming/platforms/directions/horizontal.gd
+++ assets/systems/input/platforming/platforms/directions/horizontal.gd
@@ -7,14 +7,34 @@ const gap: int = 3
 var ledges: Array[bool]
 var track: Array[float]
 
+var boxes: Array[Node2D]
+var ends: Array[bool]
+
 func calculate():
 	pass
 	
-func _cancel_ledge(body: StaticBody2D):
-	ledges[body.index] = false
+func _cancel_ledge(_body: StaticBody2D):
+	boxes.pop_back()
+	#ledges[body.index] = false
+
+func front(hero: float, box: float) -> bool: return hero > box
+func back(hero: float, box: float) -> bool: return hero < box
+
+func jump(hero: CharacterBody2D):
+	var side
+	match hero.direction:
+		"left": side = front
+		"right": side = back
+
+	for box in boxes:
+		if side.call(hero.position, box.position):
+			hero.position.x = box.position.x
 
 func protrusion(i: int, axis: float) -> bool:
 	return track[i] - gap < axis and axis < track[i] + gap
+
+func remember(body: StaticBody2D, ledge: bool):
+	if ledge: boxes.push_back(body)
 
 func intersect(body: StaticBody2D):
 	var size: int = track.size() - 1
@@ -22,6 +42,4 @@ func intersect(body: StaticBody2D):
 	while not ledge and size >= 0:
 		size -= 1
 		ledge = protrusion(size, body.position.x)
-	if ledge:
-		ledges[size] = true
-		body.index = size
+	remember(body, ledge)
--- assets/systems/input/platforming/platforms/platforms.gd
+++ assets/systems/input/platforming/platforms/platforms.gd
@@ -2,6 +2,8 @@ extends Area2D
 
 @export_range(2, 3) var edges: int = 2
 
+@onready var track = $track
+
 @export var has_center: bool = false
 @export var use_y: bool = false
 
@@ -13,14 +15,8 @@ func _ready():
 	else:
 		ledges = [false, has_center, false]
 
-func step_and_jump(hero: CharacterBody2D):
-	
-	hero.platforming.x
-	pass
-
 func _on_track(body: Node2D):
 	if body is CharacterBody2D:
-		pass
-	elif body is CharacterBody2D:
+		track.jump(body)
+	elif body is StaticBody2D:
 		track.intersect(body)
-		pass
