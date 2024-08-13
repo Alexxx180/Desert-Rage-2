d0f6a0fdedb7d791954e04e119d758b7a19b4827
Alexxx180 <alexx120702@gmail.com>
2024-08-13 07:02:08 +0300

    start working at disabling box floor detection when player no on the floor

--- assets/systems/entities/characters/hero/detectors/platform/floor/position.gd
+++ assets/systems/entities/characters/hero/detectors/platform/floor/position.gd
@@ -6,7 +6,7 @@ func _set_process(mode: ProcessMode):
 	hero.processing.movement.process_mode = mode
 
 func _should_stand_at(surface: Node2D) -> void:
-	if "surface" in surface:
+	if "surface" in surface and hero.processing.platforming.process_mode != Node.PROCESS_MODE_INHERIT:
 		surface.box.set_stand(hero)
 		surface.box.switch_stand(hero, true)
 		_set_process(Node.PROCESS_MODE_DISABLED)
