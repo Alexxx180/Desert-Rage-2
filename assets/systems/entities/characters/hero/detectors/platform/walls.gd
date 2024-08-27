00d730f88959d56571cfce867e8f4637d4e8f708
Alexxx180 <alexx120702@gmail.com>
2024-08-27 22:15:50 +0300

    fix movement -> input logic

--- assets/systems/entities/characters/hero/detectors/platform/walls.gd
+++ assets/systems/entities/characters/hero/detectors/platform/walls.gd
@@ -1,16 +1,16 @@
 extends Area2D
 
 var platforming: Node
-var movement: Node
+var input: Node
 var decisions: Node
 
 func set_control_entity(hero: CharacterBody2D):
 	var pcs = hero.processing
 	decisions = pcs.decisions
 	platforming = pcs.platforming
-	movement = pcs.movement
+	input = pcs.input
 
 func on_ledge_encounter(_surface: TileMap):
 	platforming.process_mode = decisions.will
-	platforming.perform_jump(movement.previous)
+	platforming.perform_jump(input.previous)
 	platforming.process_mode = decisions.decide(platforming.jump.feet.unstable)
