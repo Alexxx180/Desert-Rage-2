406fc9e4cc31a2a6b5362c02cc961bd7627914db
Alexxx180 <Alexxx180@github.com>
2024-09-18 03:59:21 -0400

    remove set directing redunant code

--- assets/systems/scripts/entities/characters/hero/logic/processors/input/helpers/signals.gd
+++ assets/systems/scripts/entities/characters/hero/logic/processors/input/helpers/signals.gd
@@ -9,12 +9,3 @@ static func apply(hero: CharacterBody2D) -> void:
 	HeroSignalsArea.apply(platforming, processors)
 	HeroSignalsDeployment.apply(platforming.platforms.surface, processors)
 	HeroSignalsJump.apply(hero)
-
-static func set_directing(input: Node, hero: CharacterBody2D) -> void:
-	var surface: Node = hero.logic.processors.environment.surface
-	var overview: Node = input.platfoming.jump.overview
-
-	var events: Array[Callable] = [hero.set_direction,
-		surface.tracking.set_direction, overview.redirect]
-
-	for event in events: input.directing.connect(event)
