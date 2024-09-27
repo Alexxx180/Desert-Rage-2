203f0ea88cd29d55fc397056a6dab2e73838355e
Alexxx180 <alexx120702@gmail.com>
2024-09-27 20:38:01 +0300

    connect hero set direction to platforming overview

--- assets/systems/scripts/entities/characters/hero/logic/relations/platforming/processor/jump/overview.gd
+++ assets/systems/scripts/entities/characters/hero/logic/relations/platforming/processor/jump/overview.gd
@@ -1,4 +1,8 @@
 extends Node
 
 func set_control(hero: CharacterBody2D, processor: Node) -> void:
-	processor.directions.set_directions(hero)
+	var input: Node = hero.logic.processors.input
+	var directions: Node = processor.direction
+
+	input.directing.connect(directions.overview.set_direction)
+	directions.set_directions(hero)
