23a7edad0caf4aa8a07943fe488c522bc8c381d6
Alexxx180 <Alexxx180@github.com>
2024-10-16 07:11:51 -0400

    shortened group and hero anchor code

--- assets/systems/scripts/entities/characters/group/party/deploy.gd
+++ assets/systems/scripts/entities/characters/group/party/deploy.gd
@@ -4,25 +4,19 @@ class_name HeroDeploy
 
 var _anchored: bool = false
 
-func anchor(group: Node2D, hero: CharacterBody2D, next: CharacterBody2D) -> void:
-	group.remove_child(next)
-	hero.add_child(next)
-	next.set_owner(hero)
-	hero.move.connect(next.teleport)
-	next.visible = false
-
-func unanchor(group: Node2D, hero: CharacterBody2D, next: CharacterBody2D) -> void:
-	hero.remove_child(next)
-	group.add_child(next)
-	next.set_owner(group)
-	hero.move.disconnect(next.teleport)
-	next.visible = true
+func anchor(group: Array[Node2D], next: CharacterBody2D) -> void:
+	group[0].remove_child(next)
+	group[1].add_child(next)
+	next.set_owner(group[1])
+	next.visible = _anchored
 
 func determine(group: Node2D, party: Array, order: Vector2i) -> void:
 	var hero: CharacterBody2D = party[order[0]]
 	var next: CharacterBody2D = party[order[1]]
 	if _anchored:
-		unanchor(group, hero, next)
+		anchor([hero, group], next)
+		hero.move.disconnect(next.teleport)
 	else:
-		anchor(group, hero, next)
+		anchor([group, hero], next)
+		hero.move.connect(next.teleport)
 	_anchored = !_anchored
