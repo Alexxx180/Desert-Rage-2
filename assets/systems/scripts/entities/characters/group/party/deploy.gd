9cc055c1e15c482d318e54931fbe5019f95f6ce4
Alexxx180 <Alexxx180@github.com>
2024-10-17 09:46:53 -0400

    group behavior without reset owners in progress

--- assets/systems/scripts/entities/characters/group/party/deploy.gd
+++ assets/systems/scripts/entities/characters/group/party/deploy.gd
@@ -5,13 +5,16 @@ class_name HeroDeploy
 var _anchored: bool = false
 var _available: bool = false
 
+var anchored: bool:
+	get: return _anchored
+
 func set_available(can_deploy: bool) -> void:
 	_available = can_deploy
 
 func _anchor(group: Array[Node2D], next: CharacterBody2D) -> void:
-	group[0].remove_child(next)
-	group[1].add_child(next)
-	next.set_owner(group[1])
+#	group[0].remove_child(next)
+#	group[1].add_child(next)
+#	next.set_owner(group[1])
 	next.visible = _anchored
 
 func determine(group: Node2D, party: Array, order: Vector2i) -> void:
@@ -19,10 +22,10 @@ func determine(group: Node2D, party: Array, order: Vector2i) -> void:
 	var next: CharacterBody2D = party[order[1]]
 
 	if _anchored:
-		_anchor([hero, group], next)
+		#_anchor([hero, group], next)
 		hero.move.disconnect(next.teleport)
 	elif _available:
-		_anchor([group, hero], next)
+		#_anchor([group, hero], next)
 		hero.move.connect(next.teleport)
 
 	_anchored = !_anchored
