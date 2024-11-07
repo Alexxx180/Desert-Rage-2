1a33d4cc663db8b39eb77f5e0372304a9c4f98ba
Alexxx180 <Alexxx180@github.com>
2024-11-07 07:17:56 -0500

    enable box platforming stands back

--- assets/systems/scripts/entities/characters/hero/hero.gd
+++ assets/systems/scripts/entities/characters/hero/hero.gd
@@ -20,9 +20,12 @@ func dash(force: Vector2) -> void:
 	move.emit(position)
 	print("DASHING: ", position)
 
+func _physics_process(_delta: float) -> void:
+	move_and_slide()
+
 func travel(motion: Vector2) -> void:
 	velocity = motion
-	move_and_slide()
+	#move_and_slide()
 	#if motion.y > 0 and motion.x == 0:
 	#moving.emit(velocity)
 	print("BUT MOTION IS: ", motion)
