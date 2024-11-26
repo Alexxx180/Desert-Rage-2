2627edaf17275282eaa52383886f74a06569e038
Alexxx180 <alexx120702@gmail.com>
2024-11-26 14:45:33 +0300

    add new interactable button

--- project/assets/systems/scripts/common/input/hints/cards.gd
+++ project/assets/systems/scripts/common/input/hints/cards.gd
@@ -1,3 +1,10 @@
 extends CanvasLayer
 
 @onready var hints: VBoxContainer = $hud/margin/hints
+
+@export var no_progress: bool = false
+
+func _ready() -> void:
+	if no_progress:
+		hints.motion.show_full_help()
+		hints.help.visible = true
---
+++ project/assets/systems/scripts/entities/interactables/plate/plate.gd
@@ -0,0 +1,22 @@
+extends Area2D
+
+signal pressed()
+signal release()
+
+var count: int = 0
+
+@onready var animation: AnimationPlayer = $animation
+
+func _on_press(_entity: CharacterBody2D) -> void:
+	count += 1
+	if count == 1:
+		print("PRESSED")
+		animation.play("interactables/button_down")
+		pressed.emit()
+
+func _on_release(_entity: CharacterBody2D) -> void:
+	count -= 1
+	if count == 0:
+		print("RELEASED")
+		animation.play("interactables/button_up")
+		release.emit()
