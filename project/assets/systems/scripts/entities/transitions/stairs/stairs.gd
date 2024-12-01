44d6ce5b79deb45c2709b836a3bca31cfed14df5
Alexxx180 <alexx120702@gmail.com>
2024-12-02 02:27:06 +0300

    complete downstairs full transition

--- project/assets/systems/scripts/entities/transitions/stairs/stairs.gd
+++ project/assets/systems/scripts/entities/transitions/stairs/stairs.gd
@@ -1,19 +1,25 @@
 extends Area2D
 
+var _is_finishing: bool = false
+
 @export var direction: int = 1
 
 @onready var levels: Node = $levels
 @onready var interval: Node = $interval
+@onready var transition: CanvasLayer = $transition
 
-func _finish(_hero: CharacterBody2D) -> void:
+func _finish(hero: CharacterBody2D) -> void:
+	if _is_finishing: return
+	_is_finishing = true
+	
+	#Processors.turn(hero.view, false)
+	#Processors.turn(hero.logic, false)
+	
 	Session.location["level"].x += direction
 	Session.location["level"].y = interval.part
 
 	var path: String = levels.complete_path()
-
-	print("PATH: ", path)
-	assert(ResourceLoader.exists(path))
-	get_tree().call_deferred("change_scene_to_file", path)
+	transition.start_transition(path)
 
 func _ready() -> void:
 	var level: Vector2i = Session.location["level"]
