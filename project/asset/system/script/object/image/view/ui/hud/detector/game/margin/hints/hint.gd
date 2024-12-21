aa8099e297b1e1786bbab8703285091cff8473f0
Alexxx180 <alexx120702@gmail.com>
2024-12-21 12:53:37 +0300

    preparations for preview build

--- project/asset/system/script/object/image/view/ui/hud/detector/game/margin/hints/hint.gd
+++ project/asset/system/script/object/image/view/ui/hud/detector/game/margin/hints/hint.gd
@@ -4,9 +4,18 @@ extends VBoxContainer
 
 @onready var caption: Label = $body/margin/caption
 
+func is_gamepad_connected() -> bool:
+	return Input.get_connected_joypads().size() > 0
+
 func _ready() -> void:
 	$head/margin/caption.text = help.head
-	caption.text = help.body
+	"""
+	if is_gamepad_connected():
+		caption.text = get_gamepad_hint()
+	else:
+		caption.text = help.body % help.keyboard
+	"""
+	caption.text = help.body % help.keyboard
 
 func _change_state(color: Color) -> void:
 	create_tween().tween_property(self, "modulate", color, 0.5)
@@ -22,18 +31,24 @@ func hide_delayed() -> void:
 	hide()
 	modulate = Color.WHITE
 
-func sync_control_hint(event: InputEvent) -> void:
-	var gamepad: bool = event is InputEventJoypadButton or event is InputEventJoypadMotion
+func get_gamepad_hint() -> String:
 	var device: String = Input.get_joy_name(0).to_lower()
 	var hint: String = help.body
-	
-	if gamepad and device != "":
-		var i: int = help.get_gamepad_type(device)
-		var hints: Array[String] = help.gamepad[i].hints
-		if help.gamepad.size() == 1:
-			hint = help.body % hints[0]
-		else:
-			hint = help.body % hints
+
+	if device == "": return hint % help.keyboard
+
+	var i: int = help.get_gamepad_type(device)
+	var hints: Array[String] = help.gamepad[i].hints
+
+	if help.gamepad.size() == 1:
+		hint = hint % hints[0]
 	else:
-		hint = help.body % help.keyboard
-	caption.text = hint
+		hint = hint % hints
+
+	return hint
+
+func sync_control_hint(event: InputEvent) -> void:
+	if event is InputEventJoypadButton or event is InputEventJoypadMotion:
+		caption.text = get_gamepad_hint()
+	else:
+		caption.text = help.body % help.keyboard
