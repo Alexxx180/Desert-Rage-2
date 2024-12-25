20c13b52d493347bf3d79b1471d8bbb634eda799
Alexxx180 <alexx120702@gmail.com>
2024-12-25 19:25:19 +0300

    improve platforming ground search

--- project/asset/system/script/subject/actor/group/hero/logic/detectors/platforming/platforms/surface/deployment/walls/ground.gd
+++ project/asset/system/script/subject/actor/group/hero/logic/detectors/platforming/platforms/surface/deployment/walls/ground.gd
@@ -5,7 +5,7 @@ class_name AreaDetector
 @export var offset: int = 0
 @onready var _half: Vector2 = shape.size / 2
 
-var pos: Vector2
+var pos: Vector2 = Vector2(76, 76)
 
 func set_direction(direction: Vector2) -> void:
 	var next: Vector2 = direction * (pos + _half)
