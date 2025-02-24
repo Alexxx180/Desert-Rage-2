9ed74766d08d804e8970eca626569a5e48305a41
Alexxx180 <alexx120702@gmail.com>
2025-02-24 16:28:48 +0300

    add spark diffusion and diagonal distribution

--- project/asset/system/script/subject/control/layer/activators/ability/puddle/spark.gd
+++ project/asset/system/script/subject/control/layer/activators/ability/puddle/spark.gd
@@ -1,23 +1,50 @@
 extends Node
 
-var execute: TileMapLayer
-
 @onready var particle = preload("res://asset/system/scene/subject/control/drive/rain.tscn")
+@onready var timer: Timer = $timer
 
-const PUDDLE: Vector2i = Vector2i(1, 2)
-const ID: int = 2
+enum { DROP = 1, ID = 2, DIFFUSION = 5, LENGTH = 10 }
+
+const PUDDLE: Vector2i = Vector2i(0, 2)
+const SPARK: Vector2i = Vector2i(1, 2)
+
+var execute: TileMapLayer
+var unstable: Dictionary = {} # Vector2i, int
+var path: Array[Vector2i] = []
+var size: int = 0
+
+func diagonal(map_coords: Vector2i) -> bool:
+	var i: int = 2
+	var can_spark: bool = true
+	while (can_spark and i > 0):
+		i -= 1
+		var j: int = 3
+		while (can_spark and j > -1):
+			j -= 2
+			var near: Vector2i = map_coords
+			near[i] += j
+			can_spark = not near in unstable
+	return can_spark
+
+func diffusion() -> void:
+	for puddle in unstable:
+		unstable[puddle].time -= DROP#s
+		if unstable[puddle].time <= 0:
+			unstable[puddle].atlas = PUDDLE
+			Tile.paint(execute, unstable[puddle])
+			unstable.erase(puddle)
+			size -= 1
+	if size == 0: timer.stop()
 
 func sparking(tile: Dictionary, direction: Vector2i) -> void:
-	"""
-	var spark = particle.instantiate()
-	spark.position = pos
-	spark.set_direction(direction)
-	execute.get_parent().add_child(spark)
-	"""
-	tile.atlas = PUDDLE
-	tile.id = ID
-	#print("TILE ATLAS: ", tile.atlas, " + COORDS: ", tile.coords)
-	Tile.paint(execute, tile)
+	if diagonal(tile.coords):
+		size += 1
+		tile.atlas = SPARK
+		tile.id = ID
+		tile.time = DIFFUSION#s
+		Tile.paint(execute, tile)
+		unstable[tile.coords] = tile
+	if size == 1: timer.start()
 
 func activate(pos: Vector2, direction: Vector2i) -> void:
 	var tile: Dictionary = Tile.from_pos(execute, pos)
