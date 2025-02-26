099429279190d89bdac6ade7da337a3c88fe1695
Alexxx180 <alexx120702@gmail.com>
2025-02-26 08:46:01 +0300

    start work at current path finding

--- project/asset/system/script/subject/control/layer/activators/ability/puddle/spark/current.gd
+++ project/asset/system/script/subject/control/layer/activators/ability/puddle/spark/current.gd
@@ -1,6 +1,7 @@
 extends Node
 
-var path: Array[Vector2i] = []
+var sources: Array[Array] = [] # Vector2i
+var length: Array[int] = []
 var size: int = 0
 
 func check(): pass
@@ -8,8 +9,18 @@ func check(): pass
 func fire_drain() -> void:
 	pass
 
-func connection() -> void:
+func compare_edge() -> void:
 	pass
+
+func connection(map_coords: Vector2i) -> void:
+	for i in size:
+		var j: int = length[i]
+		var path: Array[Vector2i] = sources[i]
+		var dir: Vector2i = path[j] - path[max(j - 1, 0)]
+		if dir == Vector2i.ZERO:
+			path.append(map_coords)
+		else:
+			compare_edge()
 
 func charge() -> void:
 	pass
