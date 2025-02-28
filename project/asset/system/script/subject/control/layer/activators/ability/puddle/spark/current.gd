35970ec5599d309971d99f4872f8ec1712a9c3e2
Alexxx180 <alexx120702@gmail.com>
2025-02-28 18:53:38 +0300

    start electricity current and fire drain prototyping

--- project/asset/system/script/subject/control/layer/activators/ability/puddle/spark/current.gd
+++ project/asset/system/script/subject/control/layer/activators/ability/puddle/spark/current.gd
@@ -1,26 +1,65 @@
 extends Node
 
-var sources: Array[Array] = [] # Vector2i
-var length: Array[int] = []
-var size: int = 0
+signal current_flow(map_coords: Vector2i)
 
-func check(): pass
+var current: Array[Array] = [] # Vector2i
+var size: Array[int] = []
 
-func fire_drain() -> void:
-	pass
+enum { A = -2, B = -1, LENGTH = 10 }
 
-func compare_edge() -> void:
-	pass
+func fire_drain(map_coords: Vector2i) -> void:
+	var search: bool = true
+	var direction: Vector2i
+	var i: int = current.size()
+	var j: int
+	while i > 0 and search:
+		i -= 1
+		var path: Array[Vector2i] = current[i]
+		j = path.size()
+		while j > 2 and search:
+			j -= 1
+			var delta: Vector2 = Vector2(path[j] - map_coords)
+			direction = delta.normalized()
+			search = not is_near(direction)
+
+	if not search:
+		var k: int = current[i].size()
+		while k > j:
+			current[i].remove_at(k)
+		current[i][j] += direction * -1
+
+		var track: Rect2 = get_track(current[i])
+		if track.size == Vector2.ZERO:
+			current_flow.emit(track.position)
+
+
+func at_edge(previous: Rect2i, map_coords: Vector2i) -> bool:
+	return map_coords + previous.size == previous.position
+
+func initiate_source(map_coords: Vector2i) -> void:
+	current.append([map_coords, map_coords])
+	size.append(LENGTH)
+
+func is_near(direction: Vector2i) -> bool:
+	return direction.x == 0 or direction.y == 0
+
+func search_path(map_coords: Vector2i) -> int:
+	var i: int = current.size() - 1
+	while i >= 0 and is_near(map_coords - current[i][B]): i -= 1
+	return i
+
+func get_track(path: Array[Vector2i]) -> Rect2:
+	return Rect2(path[B], Vector2(path[B] - path[A]))
 
 func connection(map_coords: Vector2i) -> void:
-	for i in size:
-		var j: int = length[i]
-		var path: Array[Vector2i] = sources[i]
-		var dir: Vector2i = path[j] - path[max(j - 1, 0)]
-		if dir == Vector2i.ZERO:
-			path.append(map_coords)
-		else:
-			compare_edge()
+	var i: int = search_path(map_coords)
+	var track: Rect2 = get_track(current[i])
 
-func charge() -> void:
-	pass
+	if track.size == Vector2.ZERO:
+		current[i][B] = map_coords
+	elif at_edge(track, map_coords):
+		current[i][B] += track.size#.normalized()
+	else:
+		current[i].append(map_coords)
+
+	current_flow.emit(map_coords)
