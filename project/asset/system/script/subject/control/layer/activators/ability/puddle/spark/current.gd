4026fcf9fbf1f124554cd7b9185e3c0348556bd3
Alexxx180 <alexx120702@gmail.com>
2025-03-01 21:25:27 +0300

    start work at fire draining

--- project/asset/system/script/subject/control/layer/activators/ability/puddle/spark/current.gd
+++ project/asset/system/script/subject/control/layer/activators/ability/puddle/spark/current.gd
@@ -1,7 +1,5 @@
 extends Node
 
-signal current_flow(pos: Vector2)
-
 const ID: int = 2
 var tiles = {
 	"PUDDLE": Vector2i(0, 2),
@@ -25,12 +23,14 @@ var execute: TileDecorator:
 
 enum { A = -2, B = -1, LENGTH = 10 }
 
+var i: int
+
 func _ready() -> void:
 	drain.flow = self
 	charge.flow.connect(puddle)
 
 func at_edge(previous: Rect2) -> bool:
-	print("EDGE: ", execute.context.coords, " - ", previous.position, " =? ", previous.size.normalized())
+	#print("EDGE: ", execute.context.coords, " - ", previous.position, " =? ", previous.size.normalized())
 	return Vector2(execute.context.coords) - previous.position == previous.size.normalized()
 
 func initiate_source(map_coords: Vector2i) -> void:
@@ -38,31 +38,36 @@ func initiate_source(map_coords: Vector2i) -> void:
 	size.append(LENGTH)
 
 func is_near(direction: Vector2i) -> bool:
-	return direction.x == 0 or direction.y == 0
+	var x: bool = direction.x == 0 and abs(direction.y) == 1 
+	var y: bool = direction.y == 0 and abs(direction.x) == 1 
+	return x or y
 
 func search_path() -> int:
 	var coords: Vector2i = execute.context.coords
-	var i: int = current.size() - 1
+	i = current.size() - 1
 	while i >= 0 and is_near(coords - current[i][B]): i -= 1
 	return i
 
-func get_delta(i: int) -> Vector2:
+func get_delta() -> Vector2:
 	var path: Array = current[i]
-	print("PATH: A = ", path[A], ", B = ", path[B])
-	print("CURRENT IS: ", current)
+	#print("PATH: A = ", path[A], ", B = ", path[B])
+	#print("CURRENT IS: ", current)
 	return Vector2(path[B] - path[A])
 
-func get_track(i: int) -> Rect2:
-	return Rect2(current[i][B], get_delta(i))
+func get_track(chain: int = i) -> Rect2:
+	return Rect2(current[chain][B], get_delta())
 
 func puddle(map_coords: Vector2i, no: int = 0) -> void:
+	if no == 1:
+		#current[i].pop_back()
+		initiate_source(map_coords)
 	execute.select(tiles.SPARK[no], ID).target(map_coords)
 	connection()
 
 func connection() -> void:
 	var map_coords: Vector2i = execute.context.coords
-	var i: int = search_path()
-	var track: Rect2 = get_track(i)
+	i = search_path()
+	var track: Rect2 = get_track()
 
 	if track.size == Vector2.ZERO:
 		current[i][B] = map_coords
@@ -71,24 +76,46 @@ func connection() -> void:
 		#return
 	elif at_edge(track):
 		print("AT EDGE!!")
-		current[i][B] += Vector2i(track.size)
+		current[i][B] += Vector2i(track.size.normalized())
 	elif not map_coords == current[i][B]:
 		current[i].append(map_coords)
 
 	execute.paint()
 	touch(map_coords)
 
-func release(direction: Vector2i, i: int, j: int) -> void:
-	var k: int = current[i].size()
-	while k >= j:
-		k -= 1
-		execute.target(current[i][k]).select(tiles.PUDDLE).paint()
-		current[i].remove_at(k)
-	current[i][k - 1] += direction * -1 # i j
+func release(map_coords: Vector2i, chain: int, joint: int) -> void:
+	var direction: Vector2i
+	var position: Vector2i
+	print("TRY [REMOVE]: CURRENT = ", chain, " UNIT = ", joint)
+	while current[chain].size() > joint:
+		position = current[chain][B]
+		direction = Vector2(current[chain][B] - current[chain][A]).normalized()
+		while position != current[chain][A]:
+			execute.target(current[chain][B]).select(tiles.PUDDLE).paint()
+			print("[REMOVE][", chain, "] ", current[chain][B])
+			position += direction
+		current[chain].pop_back()# remove_at(current[chain].size() - 1)
+	
+	position = current[chain][B]
+	direction = Vector2(current[chain][B] - current[chain][A]).normalized() * -1
+	while position != map_coords:
+		#execute.target(position).select(tiles.PUDDLE).paint()
+		execute.erase(position)
+		print("[PAINT][", chain, "] ", position, " > TO > ", map_coords)
+		# remove_at(current[chain].size() - 1)
+		position += direction
+	current[chain][B] = map_coords
+			
+	#direction = current[chain][-1] - current[chain][joint - 1]
+	#current[chain][joint - 1] = map_coords
+	#if not same:
+	#	current[chain].append(map_coords)
+	# current[chain][joint] = Vector2(map_coords) # i j
 
-	var track: Rect2 = get_track(i)
-	if track.size == Vector2.ZERO:
-		touch(track.position)
+	#var track: Rect2 = get_track(chain)
+	#if track.size == Vector2.ZERO:
+	touch(current[chain][B])
+	#touch(track.position)
 
 func touch(map_coords: Vector2i) -> void:
 	#print("EXECUTING: ", map_coords)
--- project/asset/system/script/subject/control/layer/activators/ability/puddle/spark/current/drain.gd
+++ project/asset/system/script/subject/control/layer/activators/ability/puddle/spark/current/drain.gd
@@ -7,16 +7,20 @@ func evaporation(map_coords: Vector2i) -> void:
 	var direction: Vector2i
 	var i: int = flow.current.size()
 	var j: int
+	var path: Array
 	
 	while i > 0 and search:
 		i -= 1
-		var path: Array = flow.current[i]
+		path = flow.current[i]
 		j = path.size()
 		while j > 2 and search:
 			j -= 1
 			var delta: Vector2 = Vector2(path[j] - map_coords)
 			direction = delta.normalized()
+			# search = not (direction.x == 0 or direction.y == 0)
 			search = not flow.is_near(direction)
-
-	if search: return
-	flow.release(direction, i, j)
+	if not search:
+		if map_coords != path[j]: j += 1
+		print("FIND: ", i, " - ", j)
+		#flow.execute.erase(map_coords)
+		flow.release(map_coords - direction, i, j)
