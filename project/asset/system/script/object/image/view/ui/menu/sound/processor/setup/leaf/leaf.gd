a6d6ed89a93a09fb45271d8104cacc699127b401
Alexxx180 <Alexxx180@github.com>
2025-04-01 11:58:40 -0400

    begin named and blend themes logic

--- project/asset/system/script/object/image/view/ui/menu/sound/processor/setup/leaf/leaf.gd
+++ project/asset/system/script/object/image/view/ui/menu/sound/processor/setup/leaf/leaf.gd
@@ -20,22 +20,28 @@ func _set_child(query: SoundtrackTreeQuery, child) -> SoundtrackTreeQuery:
 	return query
 
 func set_alarm(_board: BehaviorBlackboard) -> void:
-	pass
+	var query: SoundtrackTreeQuery = board.get_value("query")
+	var named: Dictionary = query.decide("name")
+	for track in named:
+		var path: String = named[track]
+		_add_child(query, _theme).set_metadata(path) # THEME TO NAMED
 
 func set_blend(_board: BehaviorBlackboard) -> void:
 	pass
 
-func set_named(_board: BehaviorBlackboard) -> void:
-	pass
+func set_named(board: BehaviorBlackboard) -> void:
+	var query: SoundtrackTreeQuery = board.get_value("query")
+	var named: Dictionary = query.decide("name")
+	for track in named:
+		var path: String = named[track]
+		_add_child(query, _theme).set_metadata(path) # THEME TO NAMED
 
-func set_trunks(board: BehaviorBlackboard) -> void:
-	var setup: Node = board.get_value("setup")
+func set_trunks(setup: Node, board: BehaviorBlackboard) -> void:
 	var query: SoundtrackTreeQuery = board.get_value("query")
 	setup.enumerate(_set_child(query, _trunk).copy("right").select("type"))
-	setup.enumerate(query.select("name"))
+	setup.enumerate(query.copy("left").select("name"))
 
-func set_branch(board: BehaviorBlackboard) -> void:
-	var setup: Node = board.get_value("setup")
+func set_branch(setup: Node, board: BehaviorBlackboard) -> void:
 	var query: SoundtrackTreeQuery = board.get_value("query")
 	setup.enumerate(_set_child(query, _branch[query.pad]).select("type"))
 
--- project/asset/system/script/object/image/view/ui/menu/sound/processor/setup/setup.gd
+++ project/asset/system/script/object/image/view/ui/menu/sound/processor/setup/setup.gd
@@ -3,8 +3,7 @@ extends Node
 @onready var behavior: BehaviorTree = $behavior
 @onready var board: BehaviorBlackboard = $blackboard
 
-func enumerate() -> void:
-	var query: SoundtrackTreeQuery = board.get_value("query")
+func enumerate(query: SoundtrackTreeQuery) -> void:
 	for key in query.get_context():
 		board.set_value("query", query.copy().select(key))
 		behavior.tick(self, board)
@@ -14,5 +13,4 @@ func set_soundtrack(ui: HFlowContainer) -> void:
 	if query.init_manifest():
 		query.set_ui(ui)
 		board.set_value("leaf", $leaf)
-		board.set_value("query", query)
-		enumerate()
+		enumerate(query)
