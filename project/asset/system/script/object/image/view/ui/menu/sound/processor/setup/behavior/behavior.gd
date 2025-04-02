25247f27aea47cf3d0417f61d771b2dcefc69dac
Alexxx180 <Alexxx180@github.com>
2025-04-02 11:51:07 -0400

    fix branch logic issues

--- project/asset/system/script/object/image/view/ui/menu/sound/processor/setup/behavior/behavior.gd
+++ project/asset/system/script/object/image/view/ui/menu/sound/processor/setup/behavior/behavior.gd
@@ -6,7 +6,6 @@ func condition(_data: Variant) -> bool: return true
 func branch(mark: Tick) -> int: return OK
 
 func tick(mark: Tick) -> int:
-	var context: Variant = mark.blackboard.get_value("query").get_context()
-	if condition(context):
+	if condition(mark.blackboard.get_value("query").context):
 		return branch(mark)
 	return FAILED
---
+++ project/asset/system/script/object/image/view/ui/menu/sound/processor/setup/branch/ui.gd
@@ -0,0 +1,21 @@
+extends Node
+
+var theme = preload("res://asset/system/scene/object/image/view/ui/menu/sound/detector/dropdown/tree/leaf/leaf.tscn")
+var fight = preload("res://asset/system/scene/object/image/view/ui/menu/sound/detector/dropdown/tree/leaf/combat.tscn")
+
+var alarm = preload("res://asset/system/scene/object/image/view/ui/menu/sound/detector/dropdown/tree/leaf/alarm.tscn")
+var named = preload("res://asset/system/scene/object/image/view/ui/menu/sound/detector/dropdown/tree/leaf/named.tscn")
+
+var trunk = preload("res://asset/system/scene/object/image/view/ui/menu/sound/detector/dropdown/tree/trunk/trunk.tscn")
+var branch: Dictionary = {
+	"left": preload("res://asset/system/scene/object/image/view/ui/menu/sound/detector/dropdown/tree/branch/left.tscn"),
+	"right": preload("res://asset/system/scene/object/image/view/ui/menu/sound/detector/dropdown/tree/branch/right.tscn")
+}
+var mix: Dictionary = {
+	"left": preload("res://asset/system/scene/object/image/view/ui/menu/sound/detector/dropdown/tree/mix/left.tscn"),
+	"right": preload("res://asset/system/scene/object/image/view/ui/menu/sound/detector/dropdown/tree/mix/right.tscn")
+}
+var blend: Dictionary = {
+	"left": preload("res://asset/system/scene/object/image/view/ui/menu/sound/detector/dropdown/tree/blend/left.tscn"),
+	"right": preload("res://asset/system/scene/object/image/view/ui/menu/sound/detector/dropdown/tree/blend/right.tscn")
+}
