f67f72ffec0b089bd1cc44353d9bdc6a9be5631e
Alexxx180 <Alexxx180@github.com>
2025-04-03 11:51:02 -0400

    fix theme leaf view show case

--- project/asset/system/script/object/image/view/ui/menu/sound/detector/tree/leaf/leaf.gd
+++ project/asset/system/script/object/image/view/ui/menu/sound/detector/tree/leaf/leaf.gd
@@ -1,15 +1,16 @@
 extends Button
 
-class_name SountrackLeaf
+class_name SoundtrackLeaf
 
 @onready var description: Label = $margin/caption/description
 @onready var metadata: Label = $margin/caption/metadata
 
 func set_metadata(track: String) -> void:
-	var slash: int = track.rfind("/")
-	var dot: int = track.rfind(".")
-	metadata.text = track.replace("\\", "/")
-	description.text = metadata.text.substr(slash).substr(dot)
+	var slash: int = track.rfind("/") + 1
+	var path: String = track.replace("\\", "/").substr(slash)
+	var dot: int = path.rfind(".")
+	metadata.text = track
+	description.text = path.substr(0, dot)
 
 func set_track_authority(ui_track: String) -> void:
 	metadata.text = ui_track
