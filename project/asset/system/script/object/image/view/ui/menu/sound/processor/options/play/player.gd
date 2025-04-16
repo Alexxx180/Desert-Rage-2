13a9827f9c57882efbee302de3c6be2603c85809
Alexxx180 <alexx120702@gmail.com>
2025-04-16 09:02:47 +0300

    start work at reset feature

--- project/asset/system/script/object/image/view/ui/menu/sound/processor/options/play/player.gd
+++ project/asset/system/script/object/image/view/ui/menu/sound/processor/options/play/player.gd
@@ -12,8 +12,8 @@ func _load_data(path: String) -> PackedByteArray:
 func load_music(track: String) -> void:
 	stop()
 	var i: int = track.rfind(".")
-	var type: String = track.substr(i).to_lower()
-	if extensions.has(type):
+	var type: String = track.substr(i + 1).to_lower()
+	if extensions.has(type) and FileAccess.file_exists(track):
 		extensions[type].data = _load_data(track)
 		stream = extensions[type]
 	play()
--- project/asset/system/script/object/plugin/global/ost.gd
+++ project/asset/system/script/object/plugin/global/ost.gd
@@ -4,24 +4,19 @@ signal update()
 
 const MANIFEST: String = "res://asset/resource/media/ost/%s.json"
 
-@onready var _copy: Dictionary = { "music": init_manifest("music") }
-@onready var _user: Dictionary = _copy
-
+var _copy: Dictionary = { "music": Defaults.DICT }
+var _user: Dictionary
 var user: Dictionary:
 	get: return _user
-
 var copy: Dictionary:
 	get: return _copy
 
 var _valid: Dictionary = { "music": false, "sound": false }
 
 func is_valid(type: String) -> bool: return _valid[type]
-
-func update_ost() -> void:
-	update.emit()
+func update_ost() -> void: update.emit()
 
 func get_file(metadata: Dictionary) -> bool:
-	# var dialog: FileDialog = FileDialog.new()
 	metadata.track = "F:/media/ost/music/songs/group/english/p-t/t/Three_Days_Grace_-_I_Hate_Everything_About_You_47958582.mp3"
 	return true
 
@@ -32,14 +27,21 @@ func get_value(ui: Dictionary, keys: Array) -> Dictionary:
 		context.ui = context.ui[key]
 	return context
 
-func _ready() -> void:
-	init_manifest("music")
+func reset() -> void:
+	_set_vault("music")
 
-func init_manifest(type: String) -> Dictionary:
+func _set_vault(theme: String) -> void:
+	var to: String = "user://%.json" % theme
+	_copy.music = _init_manifest(theme)
+	if not FileAccess.file_exists(to):
+		var from: String = MANIFEST % theme
+		var dir: DirAccess = DirAccess.new()
+		DirAccess.copy(from, to)
+
+func _init_manifest(type: String) -> Dictionary:
 	var path: String = MANIFEST % type
 	var file: FileAccess = FileAccess.open(path, FileAccess.READ)
 	var text: String = file.get_as_text()
 	var processor: JSON = JSON.new()
-	
 	_valid[type] = processor.parse(text) == OK
 	return processor.data if _valid[type] else Defaults.DICT
