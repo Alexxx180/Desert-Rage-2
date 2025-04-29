118fa7c408c0ea1a227c01670f575d10bca01ab8
Alexxx180 <Alexxx180@github.com>
2025-04-29 06:16:49 -0400

    add mouse sensitivity neighbors

--- project/asset/system/script/object/image/view/ui/menu/settings/topics/options/topic/focus/items/management.gd
+++ project/asset/system/script/object/image/view/ui/menu/settings/topics/options/topic/focus/items/management.gd
@@ -6,7 +6,12 @@ func _ready() -> void:
 	_topics.push_back(management.mouse.submit)
 	_topics.push_back(management.keyboard.options.get_parent())
 	_topics.push_back(management.gamepad.options.get_parent())
-	_items.push_back([management.mouse.submit])
+	_items.push_back(get_mouse(management.mouse))
 	_items.push_back(management.keyboard.options.get_items())
 	_items.push_back(management.gamepad.options.get_items())
 	setup(controls)
+
+func get_mouse(mouse: HSlider) -> Array[Control]:
+	grabbed.push_back(mouse)
+	mouse.set_neighbor("../mouse", "../keyboard")
+	return [mouse.submit]
