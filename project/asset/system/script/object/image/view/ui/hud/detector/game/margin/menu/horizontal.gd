55de1ca5bef6133eb1701f5445165daea7709231
Alexxx180 <alexx120702@gmail.com>
2025-06-29 17:17:24 +0300

    improve ui splitter script

--- project/asset/system/script/object/image/view/ui/hud/detector/game/margin/menu/horizontal.gd
+++ project/asset/system/script/object/image/view/ui/hud/detector/game/margin/menu/horizontal.gd
@@ -1,20 +1,25 @@
 extends HSplitContainer
 
-const RESERVE: int = 6
+@export var reserve: int = 6
 
-@export var direction: int = 1
+@export var direction: float = 0.5
 @export var node_paths: Array[String] = ["", ""]
 
 var ui_nodes: Array[Control] = []
 var previous: bool = false
+var operation: Callable
+
+func bigger(a: float, b: float) -> bool: return a > b
+func lesser(a: float, b: float) -> bool: return a < b
 
 func _ready() -> void:
 	for path in node_paths:
 		ui_nodes.append(get_node(path))
+	operation = bigger if direction < 0.0 else lesser
 
 func on_drag_end() -> void:
-	print("CURRENT: ", split_offset, "WIND: ", get_window().size.x / 2.0 * direction)
-	var current: bool = split_offset + RESERVE < get_window().size.x / 2.0 * direction
+	print("CURRENT: ", split_offset + reserve, "WIND: ", get_window().size.x * direction)
+	var current: bool = operation.call(split_offset + reserve, get_window().size.x * direction)
 	if current:
 		for node in ui_nodes: node.hide()
 	elif previous:
