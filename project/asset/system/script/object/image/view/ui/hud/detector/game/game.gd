4af746f3412023c39be9dad8c949e9b93ad0e37f
Alexxx180 <alexx120702@gmail.com>
2025-06-29 11:44:21 +0300

    major panel in game user interface improvements

--- project/asset/system/script/object/image/view/ui/hud/detector/game/game.gd
+++ project/asset/system/script/object/image/view/ui/hud/detector/game/game.gd
@@ -2,4 +2,4 @@ extends Control
 
 @onready var options: HBoxContainer = $menu/hud/ability/options
 @onready var chat: VBoxContainer = $dialog/chat
-@onready var hints: VBoxContainer = $menu/hud/metrics/priority/scroll/hints
+@onready var hints: VBoxContainer = $menu/priorities/stats/inventory/ability/controls/hints/scroll/hints
