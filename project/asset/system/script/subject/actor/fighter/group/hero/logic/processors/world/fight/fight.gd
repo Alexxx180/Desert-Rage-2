2fa6ab5f45589517883000653af16c6ecdd06358
Alexxx180 <Alexxx180@github.com>
2025-07-17 11:38:33 -0400

    start work at targeting system

--- project/asset/system/script/subject/actor/fighter/group/hero/logic/processors/world/fight/fight.gd
+++ project/asset/system/script/subject/actor/fighter/group/hero/logic/processors/world/fight/fight.gd
@@ -4,13 +4,40 @@ var close: FightRange = FightRange.new()
 var zone: FightRange = FightRange.new()
 # var all: FightRange = FightRange.new() - simply enemies list
 
-func reveal_aims() -> void:
-	print("REVEAL AIMS!")
+var party: HeroParty
+
+func _detector(entity: PhysicsBody2D) -> Node:
+	return entity.logic.detector.fight
+
+func _fight(hero: CharacterBody) -> Node:
+	return hero.logic.processor.world.fight
+
+func iterate_enemy(processor: Callable) -> void:
 	for entity in zone.area.values():
 		if entity.is_in_group("enemy"):
-			entity.logic.detector.fight.reveal_aim()
+			processor.call(entity)
+
+func reveal_aims() -> void:
+	iterate_enemy(func(entity):
+		entity.logic.detector.fight.reveal_aim()
+		add_assignee(entity, party.leader))
 
 func hide_aims() -> void:
-	for entity in zone.area.values():
-		if entity.is_in_group("enemy"):
-			entity.logic.detector.fight.hide_aim()
+	iterate_enemy(func(entity):
+		entity.logic.detector.fight.hide_aim()
+		drop_assignee(entity, party.leader))
+
+func target_accepted(enemy: CharacterBody2D) -> void:
+	# var hero: CharacterBody2D = 
+	pass
+
+func update_assignee(_party: HeroParty) -> void:
+	iterate_enemy(func(entity):
+		drop_assignee(entity, party.follower)
+		add_assignee(entity, party.leader))
+
+func add_assignee(entity: PhysicsBody2D, hero: CharacterBody) -> void:
+	_detector(entity).assign.connect(_fight(hero))
+
+func drop_assignee(entity: PhysicsBody2D, hero: CharacterBody) -> void:
+	_detector(entity).assign.disconnect(_fight(hero))
--- project/asset/system/script/subject/actor/fighter/group/hero/logic/processors/world/fight/range.gd
+++ project/asset/system/script/subject/actor/fighter/group/hero/logic/processors/world/fight/range.gd
@@ -2,13 +2,18 @@ extends RefCounted
 
 class_name FightRange
 
+signal enter(enemy: PhysicsBody2D)
+signal exit(enemy: PhysicsBody2D)
+
 var area: Dictionary = {}
 
 func enter_range(enemy: PhysicsBody2D) -> void:
 	area[enemy.get_instance_id()] = enemy
+	enter.emit(enemy)
 
 func exit_range(enemy: PhysicsBody2D) -> void:
 	area.erase(enemy.get_instance_id())
+	exit.emit(enemy)
 
 func hit_initial(enemy: PhysicsBody2D, damage: int) -> void:
 	enemy.hit.emit(damage)
