68d739bfef6d9a999087203de61ca37295eb0c7e
Alexxx180 <Alexxx180@github.com>
2025-09-03 11:43:53 -0400

    make most of the work on ui menu focus

--- project/asset/system/script/object/image/view/ui/hud/detector/game/margin/menu/split/logic.gd
+++ project/asset/system/script/object/image/view/ui/hud/detector/game/margin/menu/split/logic.gd
@@ -16,19 +16,15 @@ func show(nodes: Array) -> void:
 		if "shows" in node: node.shows()
 		else: node.show()
 
-func positive_toggle(a: float, b: float, nodes: Array) -> void:
-	if a >= b: show(nodes)
-	else: hide(nodes)
+func open_condition(direction: float) -> Callable:
+	return (func(a, b): return a <= b) if direction < 0 else (func(a, b): return a >= b)
 
-func negative_toggle(a: float, b: float, nodes: Array) -> void:
-	if a <= b: show(nodes)
-	else: hide(nodes)
+func is_opened(direction: float, a: float, b: float) -> bool:
+	return open_condition(direction).call(a, b)
 
 func drag_feedback(direction: float, proportion: float, next_offset: float, nodes: Array) -> void:
-	if direction < 0:
-		negative_toggle(next_offset, proportion, nodes)
-	else:
-		positive_toggle(next_offset, proportion, nodes)
+	if is_opened(direction, next_offset, proportion): show(nodes)
+	else: hide(nodes)
 
 func open_menu(container: SplitContainer, next: int) -> void:
 	container.split_offset = next
@@ -59,3 +55,11 @@ func smooth_back_drag(container: SplitContainer) -> void:
 func instant_drag(container: SplitContainer) -> void:
 	invariant = !invariant
 	open_menu(container, 0 if invariant else container.proportion)
+	focus_element(container)
+
+func focus_element(c: SplitContainer) -> void:
+	var nodes: Array = c.focus_nodes
+	if nodes.size() > 1 and is_opened(c.direction, c.split_offset, c.proportion):
+		nodes[1].grab_focus()
+	else:
+		nodes[0].grab_focus()
--- project/asset/system/script/object/image/view/ui/hud/detector/game/margin/menu/split/vertical/vertical.gd
+++ project/asset/system/script/object/image/view/ui/hud/detector/game/margin/menu/split/vertical/vertical.gd
@@ -3,20 +3,25 @@ extends VSplitContainer
 @export var reserve: int = 6
 @export var direction: float = 0.5
 @export var node_paths: Array[String] = ["", ""]
+@export var focus_paths: Array[String] = ["", ""]
 
 @onready var instant: Node = $instant
+@onready var tab_focus: Node = $tab_focus
 @onready var smooth: Dictionary = {
 	"direct": $smooth_direct, "back": $smooth_back
 }
 
 var logic: SplitToggleLogic = SplitToggleLogic.new()
 var ui_nodes: Array[Control] = []
+var focus_nodes: Array[Control] = []
 var proportion: float:
 	get: return get_window().size.y * direction
 
 func _ready() -> void:
 	for path in node_paths:
 		ui_nodes.append(get_node(path))
+	for path in focus_paths:
+		focus_nodes.append(get_node(path))
 
 func on_drag_end() -> void:
 	var next: float = float(split_offset + reserve)
@@ -27,6 +32,9 @@ func open_menu(next: int) -> void:
 	on_drag_end()
 
 func _input(event: InputEvent) -> void:
+	if tab_focus.listen(event):
+		logic.focus_element(self)
+		return
 	if smooth.back.listen(event):
 		logic.smooth_back_drag(self)
 		return
