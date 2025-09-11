2a68c398767a17ee70b1476b31f7206a7d55d783
Alexxx180 <Alexxx180@github.com>
2025-09-11 11:52:02 -0400

    start making group xp

--- project/asset/system/script/subject/actor/fighter/group/group.gd
+++ project/asset/system/script/subject/actor/fighter/group/group.gd
@@ -1,23 +1,52 @@
 extends Node2D
 
+signal update_stats()
+signal update_exp()
+
 @export var is_overworld: bool = false
 @export var deployed: bool = true
 @onready var camera: Camera2D = $camera
 
+const MAX_LV: int = 7
+
 var navigation: Array
 var deploy: HeroDeploy = HeroDeploy.new()
+var summary: Dictionary
+
+func _circle_level_up() -> void:
+	var lv: int = 0
+	while summary.xp >= summary.next:
+		lv += 1
+		summary.
+
+func add_exp(amount: int) -> void:
+	summary.xp += amount
+	if summary.xp >= next:
+		_circle_level_up()
+	update_exp.emit()
+	
+	upate_stats.emit()
 
 func _ready() -> void:
 	deploy.init(self, [$ray, $rock], deployed)
 	if is_overworld: camera.set_overworld()
+	summary = {
+		"xp": 0, "next": 10, "priorities": {
+			"ray": { "at": 0, "of": [0, 0, 0], "points": { "h": 9, "a": 11 }, },
+			"rock": { "at": 0, "of": [0, 0, 0], "points": { "h": 11, "a": 9 } },
+		}
+	}
+	for hero in deploy.party.heroes:
+		hero.logic.processors.stats.summary = summary
 
-func resume_input() -> void:
+func is_hud_opened() -> bool:
 	var result: bool = true
 	for n in navigation:
-		print("UI: ", n.ui.name, " op: ", not n.hud.logic.is_opened_last)
 		result = result and (not n.hud.logic.is_opened_last)
-	print("RESUME IN: ", result)
-	if not result: return
+	return not result
+
+func resume_input() -> void:
+	if is_hud_opened(): return
 	for hero in deploy.party.heroes:
 		hero.logic.processors.ui.input.resume_input()
 
--- project/asset/system/script/subject/actor/fighter/group/hero/logic/processors/stats.gd
+++ project/asset/system/script/subject/actor/fighter/group/hero/logic/processors/stats.gd
@@ -2,3 +2,5 @@ extends Node
 
 @onready var health: Node = $health
 @onready var aura: Node = $aura
+
+var summary: Dictionary
