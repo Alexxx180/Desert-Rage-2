a2f789ee2c0b3f84f998df5fb876c41d85970d7a
Alexxx180 <Alexxx180@github.com>
2025-09-11 09:10:48 -0400

    improve enemy health bar - add borders

--- project/asset/system/script/object/image/view/ui/hud/detector/game/margin/menu/status/enemy/enemy.gd
+++ project/asset/system/script/object/image/view/ui/hud/detector/game/margin/menu/status/enemy/enemy.gd
@@ -1,7 +1,7 @@
 extends PanelContainer
 
-@onready var health: ProgressBar = $health
-@onready var contested: ProgressBar = $contested
+@onready var health: ProgressBar = $hp/health
+@onready var contested: ProgressBar = $hp/contested
 @onready var caption: Label = $margin/contents/caption
 @onready var interrogate: Label = $margin/contents/interrogate
 @onready var damage: HBoxContainer = $margin/contents/damage
