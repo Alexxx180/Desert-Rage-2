34cf1d3dfceb0bb6e55bc4850b8f8ec61712c5a8
Alexxx180 <Alexxx180@github.com>
2025-11-06 10:51:59 -0500

    rework inventory items - make own classes for usables, key items, weapons and armor

--- project/asset/system/script/subject/actor/fighter/group/hero/logic/work/world/inventory/inventory.gd
+++ project/asset/system/script/subject/actor/fighter/group/hero/logic/work/world/inventory/inventory.gd
@@ -2,26 +2,15 @@ extends Node
 
 class_name HeroInventory
 
-enum { JAR = 0, WATER = 1, DUMMY = 9 }
-
 @onready var logic: Node = $logic
 @onready var chest: Node = $chest
+@onready var items: GameItems = GameItems.new()
 
-static func get_items_bank() -> Dictionary:
-	return [
-		{ "name": "Пустая банка", "short": "Позволяет хранить жидкость",
-			"description": "Находится в сундуках, их наличие значительно облегчает поход." },
-		{ "name": "Чистая вода", "short": "Восполняет ЖЗ и ДА. Ингредиент", "power": 10,
-			"description": "Используется для создания водных растворов", "tiny": "+10 ЖЗ +10 ДА" },
-		{ "icon": "🎃", "name": "Чучело", "short": "Отвлекает противников", "recipe": "Глаз + Кость + Бинт",
-			"description": "Образец собранный из подручных материалов, позволяет избежать сражения на определенное время." }
-	]
+func _slot() -> Dictionary: return { "id": JAR, "x": 0, "with": -1, "up": 0 }
 
 func _ready() -> void:
 	var d: int = JAR
 	logic.storage = [
-		{ "id": d, "x": 0 }, { "id": d, "x": 0 }, { "id": d, "x": 0 }, { "id": d, "x": 0 }, { "id": d, "x": 0 },
-		{ "id": d, "x": 0 }, { "id": d, "x": 0 }, { "id": d, "x": 0 }, { "id": d, "x": 0 }, { "id": d, "x": 0 },
-		{ "id": d, "x": 0 }, { "id": d, "x": 0 }, { "id": d, "x": 0 }, { "id": d, "x": 0 }, { "id": d, "x": 0 },
-		{ "id": d, "x": 0 }, { "id": d, "x": 0 }, { "id": d, "x": 0 }, { "id": d, "x": 0 }, { "id": d, "x": 0 },
+		_slot(), _slot(), _slot(), _slot(), _slot(), _slot(), _slot(), _slot(), _slot(), _slot(),
+		_slot(), _slot(), _slot(), _slot(), _slot(), _slot(), _slot(), _slot(), _slot(), _slot(),
 	]
--- project/asset/system/script/subject/actor/fighter/group/hero/logic/work/world/inventory/items/effect.gd
+++ project/asset/system/script/subject/actor/fighter/group/hero/logic/work/world/inventory/items/effect.gd
@@ -2,4 +2,26 @@ extends RefCounted
 
 class_name ItemsEffect
 
+func m_poison(no: int, items: GameItems, hero: CharacterBody2D) -> void:
+	pass
 
+func m_cough(no: int, items: GameItems, hero: CharacterBody2D) -> void:
+	pass
+
+func no_poison(no: int, items: GameItems, hero: CharacterBody2D) -> void:
+	pass
+
+func no_cough(no: int, items: GameItems, hero: CharacterBody2D) -> void:
+	pass
+
+func open_lock(no: int, items: GameItems, hero: CharacterBody2D) -> void:
+	pass
+
+func open_mystic(no: int, items: GameItems, hero: CharacterBody2D) -> void:
+	pass
+
+func distract(no: int, items: GameItems, hero: CharacterBody2D) -> void:
+	pass
+
+func store_water(no: int, items: GameItems, hero: CharacterBody2D) -> void:
+	pass
