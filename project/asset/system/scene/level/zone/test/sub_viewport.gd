76bc754e690df3a850629a9ffd723c53f86f1c39
Alexxx180 <Alexxx180@github.com>
2025-12-24 10:54:07 -0500

    improve magnifier base logic

--- project/asset/system/scene/level/zone/test/sub_viewport.gd
+++ project/asset/system/scene/level/zone/test/sub_viewport.gd
@@ -1,10 +1,11 @@
-extends SubViewport
+extends Control
 
-@onready var player: CharacterBody2D = get_node("../../../../../group/ray")
-@onready var camera: Camera2D = $Camera2D
+@onready var subview: SubViewport = $glass/contains/subview
+@onready var player: CharacterBody2D = get_node("..") # /../../../../group/ray
+@onready var camera: Camera2D = subview.get_node("camera")
 
 func _ready() -> void:
-	world_2d = get_tree().root.world_2d
-	
+	subview.world_2d = get_tree().root.world_2d
+
 func _physics_process(delta: float) -> void:
 	camera.position = player.position
