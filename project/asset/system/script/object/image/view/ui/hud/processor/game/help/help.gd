433c5f9edb095ee033cefe2693e171db7f0952e3
Alexxx180 <alexx120702@gmail.com>
2026-03-24 21:29:23 +0300

    continue work at new dialog system

--- project/asset/system/script/object/image/view/ui/hud/processor/game/help/help.gd
+++ project/asset/system/script/object/image/view/ui/hud/processor/game/help/help.gd
@@ -2,6 +2,12 @@ extends InputObserver
 
 var list: VBoxContainer
 
+var _level: int = 0
+var level: int:
+	get: return _level
+	set(value):
+		_level = value
+
 func add_log() -> void:
 	pass
 
@@ -9,12 +15,7 @@ func _l(a: String, b: String, entry: String):
 	if a.begins_with(entry): return true
 	return b.begins_with(entry)
 
-func ready(lvl: int):
-	var entry: String = "L" + lvl
-	var banana = ["Banana", 5]
-	my_items.insert(my_items.bsearch_custom(banana, sort_by_amount, false), banana)
-	print(my_items)
-
 func add_chat(queue: Array) -> void:
+	var lvl: int = 0
+	var entry: String = "L" + str(lvl)
 	var level: int = queue.bsearch_custom(entry, func(a, b): _l(a, b, entry))
-	
