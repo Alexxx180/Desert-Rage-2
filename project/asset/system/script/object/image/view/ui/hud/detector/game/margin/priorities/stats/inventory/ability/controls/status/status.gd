86e6dfc50fca1df813b7107a9f145d0f4cd058e8
Alexxx180 <alexx120702@gmail.com>
2026-03-31 09:05:51 +0300

    most of the hud loaded by user query

--- project/asset/system/script/object/image/view/ui/hud/detector/game/margin/priorities/stats/inventory/ability/controls/status/status.gd
+++ project/asset/system/script/object/image/view/ui/hud/detector/game/margin/priorities/stats/inventory/ability/controls/status/status.gd
@@ -1,6 +1,16 @@
 extends VBoxContainer
 
-@onready var hp: HBoxContainer = $hp
+@onready var points: HBoxContainer = $points
+
+var _hp: HBoxContainer = null
+var hp: HBoxContainer:
+	get:
+		if _hp == null:
+			var health: PackedScene = load("res://asset/system/scene/object/canvas/ui/hud/detector/game/menu/ability/controls/status/points/health.tscn")
+			var p: Control = $hp
+			p.add_sibling(health.instantiate())
+			remove_child(p)
+		return _hp
 
 var _markers: HFlowContainer = null
 var markers: HFlowContainer:
--- project/asset/system/script/object/image/view/ui/hud/detector/game/margin/priorities/stats/inventory/ability/controls/topic/status/space/title/enemies/enemies.gd
+++ project/asset/system/script/object/image/view/ui/hud/detector/game/margin/priorities/stats/inventory/ability/controls/topic/status/space/title/enemies/enemies.gd
@@ -1,17 +1,30 @@
 extends Control
 
 @export var fixed: bool = false
-@onready var space: Control = $space
 @onready var status: HBoxContainer = $status
 @onready var preset: HBoxContainer = $preset
 @onready var skills: PanelContainer = get_node("../../hints/space/scroll/stack/skills")
+
+func _update_pause() -> void:
+	if _pause == null:
+		_pause = load("res://asset/system/scene/object/canvas/ui/hud/detector/game/menu/ability/controls/topic/status/pause.tscn").instantiate()
+		add_child(_pause)
+		remove_child($holder)
+
+var _pause: HBoxContainer = null
+var pause: HBoxContainer:
+	get:
+		_update_pause()
+		return _pause
 
 var _enemy: PanelContainer = null
 var enemy: PanelContainer:
 	get:
 		if _enemy == null:
 			_enemy = load("res://asset/system/scene/object/canvas/ui/hud/detector/game/menu/ability/controls/topic/status/enemy/enemy.tscn").instantiate()
+			var space: Control = $space
 			space.add_sibling(_enemy)
+			remove_child(space)
 		return _enemy
 
 var _slots: ColorRect = null
@@ -22,4 +35,5 @@ var slots: ColorRect:
 			status.add_sibling(_slots)
 		return _slots
 
-# func _ready() -> void: status.xp.set_fixed(fixed)
+func _ready() -> void:
+	$holder.mouse_entered.connect(_update_pause) #status.xp.set_fixed(fixed)
