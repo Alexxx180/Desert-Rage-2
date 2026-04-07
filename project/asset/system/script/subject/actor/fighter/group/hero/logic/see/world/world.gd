2f21064aaed1bcb9f36eaf7cec7930f8c5def5ff
Alexxx180 <alexx120702@gmail.com>
2026-04-07 08:55:15 +0300

    decreasing physical layers count

--- project/asset/system/script/subject/actor/fighter/group/hero/logic/see/world/world.gd
+++ project/asset/system/script/subject/actor/fighter/group/hero/logic/see/world/world.gd
@@ -3,9 +3,10 @@ extends Node2D
 @onready var skills: Node2D = $skills
 @onready var ability: Node2D = $ability
 @onready var unique: Area2D = $unique
-@onready var book: ShapeCast2D = $book
+@onready var trigger: RayCast2D = $trigger
 
 func set_direction(direction: Vector2i) -> void:
+	trigger.target_position = Defaults.DIRECTION * direction
 	skills.set_direction(direction)
 	ability.set_direction(direction)
 	unique.set_direction(direction)
